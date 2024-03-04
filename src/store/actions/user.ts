5fbd3995ec470772c7068ba7f681dbcc4b329524
vijayvamja <vijayvamja2012@gmail.com>
2024-03-04 13:19:11 +0530

    Managed callback for api

--- src/store/actions/user.ts
+++ src/store/actions/user.ts
@@ -1,12 +1,33 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { UserService } from "src/api/UserService";
 import { IUser } from "src/modules/users/interface";
+import { IResponse } from "src/utils/common.type";
 
 const userService = new UserService();
 
+interface IPayload {
+  data: IUser;
+  cb?: (response: IResponse<IUser>) => void;
+}
+
 export const setUser = createAsyncThunk(
   "user/setUser",
-  async (payload: IUser) => {
-    return await userService.post(payload);
+  async (payload: IPayload) => {
+    const { data, cb } = payload;
+
+    try {
+      const response = await userService.post(data);
+
+      if (cb) {
+        cb(response);
+      }
+      return response.data;
+    } catch (err: unknown) {
+      const { error } = err as IResponse<IUser>;
+      if (cb) {
+        cb(err as IResponse<IUser>);
+      }
+      throw error;
+    }
   },
 );
