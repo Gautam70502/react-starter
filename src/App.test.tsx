655a58737260f17788a3e28a9e7f745711a81a78
Gautam7502 <101510617+Gautam70502@users.noreply.github.com>
2024-05-13 15:11:30 +0530

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react'; // Importing RenderResult for type definitions
+import { render,screen } from '@testing-library/react';
 import App from './App';
 
 // A simple test case to check if the App component renders without crashing
 test('renders App component', () => {
-  const { container }: RenderResult = render(<App />); // Adding type annotation for container
-  expect(container).toBeInTheDocument(); // Using TypeScript type checking
+  render(<App />);
+  const linkelement = screen.getByText(/hello react/i);
+  expect(linkelement).toBeInTheDocument();
 });
 
 // You can write more test cases to verify different aspects of your component's behavior
