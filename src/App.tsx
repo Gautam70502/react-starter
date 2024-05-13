ae4d44a35fa283c7d798915314c443982f294373
Gautam7502 <101510617+Gautam70502@users.noreply.github.com>
2024-05-13 17:06:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes,BrowserRouter } from "react-router";
 
 import classes from "./App.module.scss";
 import PrivateLayout from "./layout/PrivateLayout";
@@ -8,11 +8,13 @@ import PublicLayout from "./layout/PublicLayout";
 const App = () => {
   return (
     <div className={classes.app}>
+      <BrowserRouter basename={window.location.pathname || ''}>
       <Routes>
         <Route path="/private/*" element={<PrivateLayout />} />
         <Route path="/*" element={<PublicLayout />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+        </BrowserRouter>
     </div>
   );
 };
