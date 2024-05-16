73521e5d9877fef7021206f79875f62da8ef580b
Gautam70502 <gautamrathod70502@gmail.com>
2024-05-16 11:01:28 +0530

    updated dockerfile

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render,screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
 // A simple test case to check if the App component renders without crashing
-test('renders App component', () => {
+test("renders App component", () => {
   render(<App />);
   const linkelement = screen.getByText(/hello react/i);
   expect(linkelement).toBeInTheDocument();
