afef2c26bb1f0ca2fccb5d0317c43efc0219f28b
Gautam7502 <101510617+Gautam70502@users.noreply.github.com>
2024-05-17 11:24:46 +0530

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,7 +25,7 @@ const Home = () => {
     <div>
       Welcome
       <div>
-        <button onClick={handleClick}>Log in Again</button>
+        <button onClick={handleClick}>Log in Again again</button>
       </div>
     </div>
   );
